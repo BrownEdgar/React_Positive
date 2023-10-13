c906cfe347b71424fb661b294bf5abf8b80f8d43
arsenaghayan <arsenaghayann@gmail.com>
2023-10-14 01:41:41 +0400

    add homework

--- src/Users/Arsen_V/App.jsx
+++ src/Users/Arsen_V/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Toma</h1>
+      <h1>
+        
+               My login was successful
+
+      </h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/lesson5/App'
+import App from './Users/Arsen_V/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

4b75beae70af775d9caface8d7b19c0a567ab691
arsenaghayan <arsenaghayann@gmail.com>
2023-10-14 10:49:06 +0400

    in class scss

--- src/Users/Arsen_V/App.jsx
+++ src/Users/Arsen_V/App.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import { useState } from 'react'
+import "./App.scss"
 
 export default function App() {
-  return (
-    <div>
-      <h1>
-        
-               My login was successful
+const [data, setSata] = useState([
+  {id:1, name: 'Bruce li', age: 34, role: 'actor', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqc3UXqURR2Zuxj-M1N9cwHOsOJEwaj3salQ&usqp=CAU'},
+  {id:2, name: 'Jan Clode Ban Dame', age: 55, role: 'actor'},
+  {id:3, name: 'Jason Statham', age: 55, role: 'actor'},
+  {id:4, name: 'Swayne Douglas Johnson', age: 34, role: 'actor'},
+])
 
-      </h1>
+  return (
+    <div className='App'>
+      <h1 className='App-title'>Our Actors</h1>
+      <div className='App-container'>
+        {
+          data.map(elem => {
+            return(
+              <div key={elem.id}>
+                {elem.image ? <img src={elem.image} alt="" /> : null}
+                <h3>{elem.name}</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }

5d69d38b4e419e2ca3f56848381cec69a68c5984
lusine2121 <lusine.tonoyan1993@gmail.com>
2023-10-14 06:39:45 +0400

    homework

--- src/Users/Lusine/App.jsx
+++ src/Users/Lusine/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <h1>Toma</h1>
+      <h1>Lusine</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, odit eveniet sit quia totam deleniti similique doloremque omnis modi iste tenetur velit rem nam officia quam ex obcaecati accusamus illum?
+      </p>
+      <button onClick={handleClick}>click me</button>
     </div>
   )
 }

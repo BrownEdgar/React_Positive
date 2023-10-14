e3a34156f4fe93e63f302a5e59e738e9b6ccff19
lusine2121 <lusine.tonoyan1993@gmail.com>
2023-10-14 06:49:16 +0400

    add homework

--- src/Users/Lusine/App.jsx
+++ src/Users/Lusine/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <div>
       <h1>Lusine</h1>

8e12e55bbd31660f0850d9abcd36f1539f49146d
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-10-21 08:57:59 +0400

    added axios example

--- src/Users/Hayk/homework1/App.jsx
+++ src/Users/Hayk/homework1/App.jsx
@@ -3,30 +3,30 @@ import { useEffect, useState } from "react"
 import "./App.scss"
 
 export default function App() {
- const [info, setInfo] = useState([])
- useEffect(() => {
-   fetch("https://jsonplaceholder.typicode.com/todos")
-   .then(res => res.json())
-   .then(data => setInfo(data))
- },[])
+  const [info, setInfo] = useState([])
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(res => res.json())
+      .then(data => setInfo(data))
+  }, [])
 
- const handleDelete = (index) =>{
-   setInfo(info.toSpliced(index,1))
- }
+  const handleDelete = (index) => {
+    setInfo(info.toSpliced(index, 1))
+  }
 
   return (
     <div className="Container">
-   {
-    info.map((elem,index)=>{
-      return (
-         <div className="Box" key={elem.id} >
-          <p className="Box_id">{elem.id}</p>
-          <h3 className="Box_title">{elem.title}</h3>
-          <p  className="Box_delete"  onClick={() => handleDelete(index)}>&#10008;</p>
-         </div>
-      )
-    })
-   }
+      {
+        info.map((elem, index) => {
+          return (
+            <div className="Box" key={elem.id} >
+              <p className="Box_id">{elem.id}</p>
+              <h3 className="Box_title">{elem.title}</h3>
+              <p className="Box_delete" onClick={() => handleDelete(index)}>&#10008;</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }

dd7c0484bb38fce26a24d49c423b03db697093cf
arsenaghayan <arsenaghayann@gmail.com>
2023-11-02 00:41:09 +0400

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './Users/Arsen_V/Form/App2'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root'))

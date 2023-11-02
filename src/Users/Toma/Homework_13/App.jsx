fafc467f6c4771f12129a29413acbed2d193ba3e
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-02 09:11:57 +0400

    added form example

--- src/Users/Toma/Homework_13/App.jsx
+++ src/Users/Toma/Homework_13/App.jsx
@@ -6,52 +6,48 @@ export default function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const { username, password } = e.target
+    const user = {
+      id: Date.now(),
+      username: username.value,
+      password: password.value
+    };
 
-  const user = {
-    id: Date.now(),
-    username: username.value,
-    password: password.value
-  };
-
-   setUsers([...users, user]);
+    setUsers([...users, user]);
     e.target.reset();
   };
 
   const checkUser = (e) => {
     e.preventDefault();
     const checkUsername = e.target.checkUsername.value;
+
+    const userExists = users.some((user) => user.username === checkUsername);
+    alert(`${checkUsername} ${!userExists && 'does not'} exist in the list.`);
+
     e.target.reset();
-
-  const userExists = users.some((user) => user.username === checkUsername);
-
-  if (userExists) {
-    alert(`${checkUsername} exists in the list.`);
-    } else {
-    alert(`${checkUsername} does not exist in the list.`);
-    }
   };
 
   return (
     <div className="App">
       <div className='App-content'>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username</label>
-      <input type="text" id="username" required />
-      <label htmlFor="password">Password</label>
-      <input type="text" id="password" required />
-      <input type="submit" value="Add" />
-    </form>
-      <p>
-        {JSON.stringify(users)}
-      </p>
-    </div>
-    <div className='App-cont'>
-     
-    <form onSubmit={checkUser}>
-      <label htmlFor="checkUsername">Check Username</label>
-      <input type="text" id="checkUsername" required />
-      <input type="submit" value="Check" />
-    </form>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" required />
+          <label htmlFor="password">Password</label>
+          <input type="text" id="password" required />
+          <input type="submit" value="Add" />
+        </form>
+        <p>
+          {JSON.stringify(users)}
+        </p>
+      </div>
+      <div className='App-cont'>
+
+        <form onSubmit={checkUser}>
+          <label htmlFor="checkUsername">Check Username</label>
+          <input type="text" id="checkUsername" required />
+          <input type="submit" value="Check" />
+        </form>
       </div>
     </div>
   );

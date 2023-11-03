17bc71564eed19a42095198728541200df743fe8
ArsenGabrielyan444 <arsen.gabrielyan.2000@list.ru>
2023-11-03 22:24:03 +0300

    last homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './Users/Arsen_E/easy homework 1/App'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root'))

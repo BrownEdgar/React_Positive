89ece0c66f6aa1965e71855271a340885de247f0
DarbinyanHayk <darbinyan-22@mail.ru>
2023-11-11 09:21:23 +0400

    homework11 npm i react-Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "./Users/Edgar/React-router/App";
-=======
-<<<<<<< HEAD
-import App from "./Users/Toma/React-router/App";
-=======
 import App from "./App";
->>>>>>> e6fe679fbb36b3f23fdc87c963f9c09cd93eb85f
->>>>>>> refs/remotes/origin/main
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<<<<<<< HEAD
   <Router>
     <App />
   </Router>
-=======
-<Router>
-  <App />
-</Router>
-
->>>>>>> refs/remotes/origin/main
-)
+);

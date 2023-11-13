5d432479098734c66643beb3286fdfabb16afb25
Toma-357 <tamaragyulasaryan@gmail.com>
2023-11-14 00:23:39 +0400

    add homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "./Users/Toma/React-router/App";
-=======
-import App from "./App";
->>>>>>> e6fe679fbb36b3f23fdc87c963f9c09cd93eb85f
+import App from "./Users/Toma/React-router2/App";
 import "./index.css";
-import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-<Router>
-  <App />
-</Router>
 
-)
+ReactDOM.createRoot(document.getElementById("root")).render( <App />)

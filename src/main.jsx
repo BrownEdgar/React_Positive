a0b52272b4b916a8ee58e7415467884ff0b31b32
Roleni21 <lusine.tonoyan1993@gmail.com>
2023-11-16 06:53:22 +0400

    add homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Lusine/React-router/App";
+import App from "./Users/Lusine/Hooks/useCallback & useMemo/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />)

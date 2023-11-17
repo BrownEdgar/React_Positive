580c458e4981b7b337307781c14d25541c43a1ea
Toma-357 <tamaragyulasaryan@gmail.com>
2023-11-18 00:24:01 +0400

    add homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Lusine/React-router/App";
+import App from "./Users/Toma/Homework_21/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />)

be051adf4f5b8183a66b1841eec2dfbf2991a152
Roleni21 <lusine.tonoyan1993@gmail.com>
2023-11-17 21:40:56 +0400

    add homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Edgar/Hooks/useTransition/App";
+import App from "./Users/Edgar/Hooks/useCalback & useMemo/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

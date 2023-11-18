e58a4a74a2609f58afbcf524aa6d92d326a8f245
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-11-18 09:29:57 +0400

    merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "./Users/Lusine/Homework21_useTransition/App";
-import "./index.css";
-=======
 import App from "./Users/Toma/Homework_21/App";
 
->>>>>>> 5e682184fe5654ed2641d57928038875736e479b
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

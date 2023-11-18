634b7e9a429f49afca8dcd1abb68e80497609b26
Roleni21 <lusine.tonoyan1993@gmail.com>
2023-11-18 07:12:43 +0400

    add homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./Users/Edgar/Hooks/useTransition/App";
+import App from "./Users/Lusine/Homework21_useTransition/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);

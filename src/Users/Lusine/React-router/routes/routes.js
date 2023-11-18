d0473cc8054990780c7a8015467caa486132fd3f
DarbinyanHayk <darbinyan-22@mail.ru>
2023-11-18 09:33:11 +0400

    create branch

--- src/Users/Lusine/React-router/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const ROUTES = {
-    HOME: '/',
-    ABOUT: 'about',
-    BLOG: 'blog',
-    CONTACT:  'contact',
-    COMMENT:  'comments/:commentsId', //-
-}
-
-export default ROUTES 
--- src/Users/Toma/React-router/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const ROUTES = {
-    TODOS: '/todos',
-    COMMENTS: '/coments',
-    USERS: '/users',
-    POSTS: '/posts'
-}
-export default ROUTES

cd9ed046ab11e4c5b5d2e28c29a05c71c7fbc745
ArsenGabrielyan444 <arsen.gabrielyan.2000@list.ru>
2023-11-27 23:05:45 +0300

    add asyncron redux homework

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterSlice from '../features/counter/counterSlice';
 import episodesSlice from '../features/counter/episodes/episodesSlice';
+import postSlice from '../features/posts/postSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterSlice,
-     episodes:episodesSlice
+     episodes:episodesSlice,
+     posts:postSlice
   }
 })
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './components/posts/Posts'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
